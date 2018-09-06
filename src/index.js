fed28bac3038f349497b30516296fd2e119272d5
Tyler Collier <tyler.collier+github@devmounta.in>
2018-09-06 10:26:31 -0700

    Add hot reloading

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { AppContainer } from 'react-hot-loader';
+
+// Wrap the rendering in a function:
+const render = () => {
+  ReactDOM.render(
+    // Wrap App inside AppContainer
+    <AppContainer>
+          <App />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+// Render once
+render();
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render();
+  });
+}
